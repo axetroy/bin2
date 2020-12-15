7bdf68165df98947c2bed7d4a0c2ddff43e6305c
axetroy <axetroy.dev@gmail.com>
2020-12-16 01:33:10 +0800

    update

--- cmd/bin2/main.go
+++ cmd/bin2/main.go
@@ -1,50 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"regexp"
 	"strings"
 
 	"github.com/axetroy/bin2"
 )
-
-func getLatestRelease(owner string, repo string) (string, error) {
-	var (
-		err error
-	)
-
-	url := fmt.Sprintf("https://github.com/%s/%s/releases", owner, repo)
-
-	res, err := http.Get(url)
-
-	if err != nil {
-		return "", err
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		return "", err
-	}
-
-	reg := regexp.MustCompile(fmt.Sprintf(`/%s/%s/releases/download/(.*)/.*\.tar.gz`, owner, repo))
-	// reg := regexp.MustCompile(fmt.Sprintf(`/%s/%s/releases/tag/v([a-z\d\.]+)`, owner, repo))
-
-	matchers := reg.FindStringSubmatch(string(body))
-
-	if len(matchers) == 0 {
-		return "", errors.New("not found latest version")
-	}
-
-	version := matchers[1]
-
-	return version, nil
-}
 
 func handler(w http.ResponseWriter, r *http.Request) error {
 	var (
@@ -62,17 +26,7 @@ func handler(w http.ResponseWriter, r *http.Request) error {
 
 	owner := arr[0]
 	repo := strings.Join(arr[1:], "/")
-
 	userAgent := r.Header.Get("user-agent")
-
-	// If no version is specified, the latest version is used
-	if version == "" {
-		version, err = getLatestRelease(owner, repo)
-
-		if err != nil {
-			return err
-		}
-	}
 
 	script, err := bin2.Generate(owner, repo, version, binaryName, userAgent)
 
